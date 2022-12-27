bdc0b50cd6ea4c1d004f7bd6d4e65bcc3c61ae67
rahali1997 <74298348+rahali1997@users.noreply.github.com>
2022-12-27 09:04:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import express from "express"
 
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.send('Server is Running')
+app.get('/rest1',(req,res)=>{
+    res.send('First Api is Running...')
 })
 
-app.get('/api/all',(req,res)=>{
-    res.status(200).json({res:"hello"})
-})
+
 
 
 app.listen(5000,()=>console.log('server is running'))
